f6a1dfb4d09e9b8fab19d697cefdd698bde9231a
Anthony You <you.anthony@yahoo.com>
2021-12-15 16:16:46 +0100

    add position and checkbox components

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,4 +2,5 @@ class Todo < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3 }
   enum status: { unchecked: 0, checked: 1 }
   belongs_to :user
+  default_scope { order(position: :asc) }
 end
