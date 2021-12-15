527d8274d7c9ced53900a7c8a4d864f4586062c5
Anthony You <you.anthony@yahoo.com>
2021-12-15 15:10:39 +0100

    add devise_no_pass file with config, to do later

---
+++ config/initializers/devise_no_pass.rb
@@ -0,0 +1,15 @@
+require 'devise/strategies/authenticatable'
+
+module Devise
+  module Strategies
+    class DeviseNoPass < Authenticatable
+      def authenticate!
+        return super unless params[:customer_sign_in]
+        user = User.find_by_email(params[:customer_sign_in])
+        user ? success!(user) : raise
+      end
+    end
+  end
+end
+
+Warden::Strategies.add(:devise_no_pass, Devise::Strategies::DeviseNoPass)
